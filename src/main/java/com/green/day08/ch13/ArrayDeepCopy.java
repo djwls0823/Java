fa6da6c7a24641b4de1673f12be144660e14d350
Lee eo jin <djwls7777@gmail.com>
2024-10-17 17:33:40 +0900

    24.10.17

--- src/main/java/com/green/day08/ch13/ArrayDeepCopy.java
+++ src/main/java/com/green/day08/ch13/ArrayDeepCopy.java
@@ -8,6 +8,13 @@ public class ArrayDeepCopy {
         for (int i = 0; i < arr.length; i++) {
             copyArr[i] = arr[i];
         }
+
+        System.out.println("arr == copyArr: " + (arr == copyArr));
+        //참조변수끼리의 == 비교는 동일성(같은 주소값)을 묻는다.
+
+        int[] arr2 = copyArr; //shallow copy (얕은 복사, 주소값 복사)
+        System.out.println("arr2 == copyArr: " + (arr2 == copyArr));
+
         for (int i = 0; i < arr.length; i++) {
             System.out.printf("arr[%d]: %d\n", i, arr[i]);
         }
