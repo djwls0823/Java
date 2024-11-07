ba4e021e39a0b10ee1c7c6e8c5033ca6ce50641e
Lee eo jin <djwls7777@gmail.com>
2024-11-07 18:02:03 +0900

    24.11.07

---
+++ src/main/java/com/green/database/Board.java
@@ -0,0 +1,58 @@
+package com.green.database;
+
+public class Board {
+    private int boardId;
+    private String title;
+    private String contents;
+    private String writer;
+    private String createdAt;
+    private String updateAt;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdateAt(String updateAt) {
+        this.updateAt = updateAt;
+    }
+}
